989f183f719bb551f6c028700677dd4572547bdf
Luise Koehler <luise.koehler@bbaw.de>
2022-07-12 14:54:48 +0200

    Adds type hints

--- tei_transform/filename_element_observer.py
+++ tei_transform/filename_element_observer.py
@@ -1,7 +1,10 @@
+from lxml import etree
+
+
 class FilenameElementObserver:
     xpattern = "//filename"
 
-    def observe(self, node) -> bool:
+    def observe(self, node: etree._Element) -> bool:
         if node.xpath(self.xpattern):
             return True
         return False
--- tei_transform/id_attribute_observer.py
+++ tei_transform/id_attribute_observer.py
@@ -1,7 +1,10 @@
+from lxml import etree
+
+
 class IdAttributeObserver:
     xpattern = "//*[@id]"
 
-    def observe(self, node) -> bool:
+    def observe(self, node: etree._Element) -> bool:
         if node.xpath(self.xpattern):
             return True
         return False
