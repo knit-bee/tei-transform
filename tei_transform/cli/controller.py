ac3dc43008cd9e3933519db90e79740897a9d239
Luise Koehler <luise.koehler@bbaw.de>
2022-07-20 12:09:55 +0200

    Implement cli argument parsing

--- tei_transform/cli/controller.py
+++ tei_transform/cli/controller.py
@@ -9,4 +9,30 @@ class TeiTransformController:
         self.use_case = use_case
 
     def process_arguments(self, arguments: List[str]) -> None:
-        pass
+        parser = argparse.ArgumentParser(
+            description="""Parse an xml-file that has some errors (that make it
+            invalid according to TEI P5) and apply transformations the file
+            content."""
+        )
+        parser.add_argument(
+            "file",
+            help="File to process",
+            type=str,
+        )
+        parser.add_argument(
+            "--transformation",
+            "-t",
+            help="""Observer plugins that should be used to transform the file
+            content. If no plugin is passed, the default setting will be used
+            ('schemalocation, id-attribute, teiheader, notestmt, filename-element')""",
+            nargs="+",
+            default=[
+                "schemalocation",
+                "id-attribute",
+                "teiheader",
+                "notestmt",
+                "filename-element",
+            ],
+        )
+        args = parser.parse_args(arguments)
+        self.use_case.process(CliRequest(file=args.file, observers=args.transformation))
