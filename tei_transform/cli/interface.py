f93c2941bb32e3dada7f01653a2ba62f84c3539a
Luise Koehler <luise.koehler@bbaw.de>
2022-07-27 12:52:02 +0200

    Add logging setup

--- tei_transform/cli/interface.py
+++ tei_transform/cli/interface.py
@@ -1,7 +1,15 @@
+import logging
 import sys
 
 from tei_transform.cli.controller import TeiTransformController
 from tei_transform.cli.use_case import TeiTransformationUseCaseImpl
+
+logging.basicConfig(
+    filename="tei-transform.log",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s: %(message)s",
+)
+logger = logging.getLogger()
 
 
 def main() -> None:
