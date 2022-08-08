4ee9ffdb7a12b22614f9b6d5f41f94ef2c275613
Luise Koehler <luise.koehler@bbaw.de>
2022-08-08 15:12:28 +0200

    Turn TeiNamespaceObserver into dummy

--- tei_transform/tei_namespace_observer.py
+++ tei_transform/tei_namespace_observer.py
@@ -4,9 +4,13 @@ from tei_transform.abstract_node_observer import AbstractNodeObserver
 
 
 class TeiNamespaceObserver(AbstractNodeObserver):
+    """Dummy plugin for TEI element
+
+    This observer doen't do anything, action is performed in TeiTransformer.
+    Use this as plugin anyway, if you want to add xml namespace for TEI
+    (http://www.tei-c.org/ns/1.0) to a file."""
+
     def observe(self, node: etree._Element) -> bool:
-        if node.tag == "TEI":
-            return True
         return False
 
     def transform_node(self, node: etree._Element) -> None:
--- tests/test_tei_namespace_observer.py
+++ tests/test_tei_namespace_observer.py
@@ -9,30 +9,7 @@ class TeiNamespaceObserverTester(unittest.TestCase):
     def setUp(self):
         self.observer = TeiNamespaceObserver()
 
-    def test_observer_returns_true_for_matching_node(self):
-        node = etree.XML("<TEI/>")
-        result = self.observer.observe(node)
-        self.assertTrue(result)
-
-    def test_observer_returns_false_for_not_matching_nodes(self):
-        node = etree.XML("<TEI xmlns='http://www.tei-c.org/ns/1.0'/>")
-        result = self.observer.observe(node)
-        self.assertEqual(result, False)
-
-    def test_observer_finds_matching_nodes(self):
-        matching_elements = [
-            etree.XML("<TEI></TEI>"),
-            etree.XML("<TEI><teiHeader/><text/></TEI>"),
-            etree.XML(
-                "<TEI><teiHeader><fileDesc>text</fileDesc></teiHeader><text>some text</text></TEI>"
-            ),
-        ]
-        for element in matching_elements:
-            result = [self.observer.observe(node) for node in element.iter()]
-            with self.subTest():
-                self.assertEqual(sum(result), 1)
-
-    def test_observer_ignores_non_matching_nodes(self):
+    def test_observer_returns_false_for_any_node(self):
         elements = [
             etree.XML("<TEI xmlns='http://www.tei-c.org/ns/1.0'></TEI>"),
             etree.XML(
