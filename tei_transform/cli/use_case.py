a74b0f3dfe491067aa381d06dfeaad6fec6f848f
Luise Koehler <luise.koehler@bbaw.de>
2022-08-22 16:54:50 +0200

    Dir processing draft

--- tei_transform/cli/use_case.py
+++ tei_transform/cli/use_case.py
@@ -1,11 +1,14 @@
 import os
 from dataclasses import dataclass
-from typing import List, Optional, Protocol
+from typing import Generator, List, Optional, Protocol, Union
 
 from lxml import etree
 
 from tei_transform.observer_constructor import ObserverConstructor
-from tei_transform.revision_desc_change import construct_change_from_config_file
+from tei_transform.revision_desc_change import (
+    RevisionDescChange,
+    construct_change_from_config_file,
+)
 from tei_transform.tei_transformer import TeiTransformer
 from tei_transform.xml_tree_iterator import XMLTreeIterator
 
@@ -19,7 +22,9 @@ class CliRequest:
 
 
 class TeiTransformationUseCase(Protocol):
-    def process(self, request: CliRequest) -> None:
+    def process(
+        self, request: CliRequest
+    ) -> Union[etree._Element, Generator[etree._Element, None, None]]:
         ...
 
 
@@ -28,7 +33,9 @@ class TeiTransformationUseCaseImpl:
     Use case that is called by console script.
     """
 
-    def process(self, request: CliRequest) -> etree._Element:
+    def process(
+        self, request: CliRequest
+    ) -> Union[etree._Element, Generator[etree._Element, None, None]]:
         """
         Processes cli arguments and applies them for the transformation
         of an xml tree. The transformed tree is returned.
@@ -39,20 +46,49 @@ class TeiTransformationUseCaseImpl:
         transformer = TeiTransformer(
             xml_iterator=tree_iterator, list_of_observers=observer_list
         )
-        new_root = transformer.perform_transformation(request.file_or_dir)
+        change = None
         if request.config is not None:
             change = construct_change_from_config_file(request.config)
-            if transformer.xml_tree_changed() and change is not None:
-                transformer.add_change_to_revision_desc(new_root, change)
         if request.output is not None:
             os.makedirs(request.output, exist_ok=True)
-            if new_root is not None:
-                output_file = os.path.join(
-                    request.output, os.path.basename(request.file_or_dir)
-                )
-                new_root.getroottree().write(
-                    output_file,
-                    xml_declaration=True,
-                    encoding="utf-8",
-                )
+        if os.path.isfile(request.file_or_dir):
+            return self._process_file(
+                transformer,
+                file=request.file_or_dir,
+                output_dir=request.output,
+                revision_entry=change,
+            )
+        elif os.path.isdir(request.file_or_dir):
+            for root, dirs, files in os.walk(request.file_or_dir):
+                output_sub_dir = None
+                if request.output is not None:
+                    output_sub_dir = os.path.join(
+                        request.output, os.path.relpath(root, start=request.file_or_dir)
+                    )
+                    os.makedirs(output_sub_dir, exist_ok=True)
+                for file in files:
+                    self._process_file(
+                        transformer,
+                        file=os.path.join(root, file),
+                        output_dir=output_sub_dir,
+                        revision_entry=change,
+                    )
+
+    def _process_file(
+        self,
+        transformer: TeiTransformer,
+        file: str,
+        output_dir: Optional[str],
+        revision_entry: Optional[RevisionDescChange] = None,
+    ) -> etree._Element:
+        new_root = transformer.perform_transformation(file)
+        if transformer.xml_tree_changed() and revision_entry is not None:
+            transformer.add_change_to_revision_desc(new_root, revision_entry)
+        if output_dir is not None and new_root is not None:
+            output_file = os.path.join(output_dir, os.path.basename(file))
+            new_root.getroottree().write(
+                output_file,
+                xml_declaration=True,
+                encoding="utf-8",
+            )
         return new_root
