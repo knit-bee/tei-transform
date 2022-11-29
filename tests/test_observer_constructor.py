14cb67dbe28d6567ff177193a68bb8c9af62a48d
Luise Koehler <luise.koehler@bbaw.de>
2022-11-29 13:30:59 +0100

    Refactor tests

--- tests/test_observer_constructor.py
+++ tests/test_observer_constructor.py
@@ -6,24 +6,24 @@ from tei_transform.observer_constructor import InvalidObserver, ObserverConstruc
 
 
 class ObserverConstructorTester(unittest.TestCase):
+    def setUp(self):
+        self.constructor = ObserverConstructor()
+
     def test_observer_construction(self):
-        constructor = ObserverConstructor()
-        test_observer = constructor.construct_observers(["filename-element"])[0]
+        test_observer = self.constructor.construct_observers(["filename-element"])[0]
         self.assertTrue(isinstance(test_observer, FilenameElementObserver))
 
     def test_exception_raised_if_observer_is_not_registered_as_plugin(self):
-        constructor = ObserverConstructor()
         self.assertRaises(
-            InvalidObserver, constructor.construct_observers, ["fake-observer"]
+            InvalidObserver, self.constructor.construct_observers, ["fake-observer"]
         )
 
     def test_exception_raised_if_observer_is_invalid(self):
-        constructor = ObserverConstructor()
-        constructor.plugins_by_name["fake-observer"] = metadata.EntryPoint(
+        self.constructor.plugins_by_name["fake-observer"] = metadata.EntryPoint(
             name="fake-observer",
             group="node-observer",
             value="tests.test_tei_transformer:FakeObserver",
         )
         self.assertRaises(
-            InvalidObserver, constructor.construct_observers, ["fake-observer"]
+            InvalidObserver, self.constructor.construct_observers, ["fake-observer"]
         )
