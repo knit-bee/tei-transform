6fea415c985a67ba175606a8f4b9dd85f3ab1386
Luise Koehler <luise.koehler@bbaw.de>
2023-01-09 16:16:47 +0100

    WIP: implement observer action for LonelyCellObserver

--- tei_transform/observer/lonely_cell_observer.py
+++ tei_transform/observer/lonely_cell_observer.py
@@ -26,4 +26,26 @@ class LonelyCellObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        parent = node.getparent()
+        prev_sibling = node.getprevious()
+        node_index = parent.index(node)
+        if prev_sibling is None or (
+            prev_sibling != self._new_row and prev_sibling != self._new_table
+        ):
+            new_row = create_new_element(node, "row")
+            parent.insert(node_index, new_row)
+            self._new_row = new_row
+        self._new_row.append(node)
+        if etree.QName(parent).localname != "table":
+            if self._new_table is None or self._new_row not in self._new_table:
+                new_table = create_new_element(node, "table")
+                parent.insert(node_index, new_table)
+                new_table.append(self._new_row)
+                self._new_table = new_table
+                parent = new_table
+        if node.tail is not None and node.tail.strip():
+            if parent.tail is None:
+                parent.tail = node.tail.strip()
+            else:
+                parent.tail += f" {node.tail.strip()}"
+            node.tail = None
