2910761158be541b0cdb00da512fddcdbd13aebc
Luise Koehler <luise.koehler@bbaw.de>
2023-01-09 11:50:01 +0100

    Implement observer action

--- tei_transform/observer/lonely_row_observer.py
+++ tei_transform/observer/lonely_row_observer.py
@@ -1,8 +1,19 @@
+from typing import Optional
+
 from lxml import etree
+
 from tei_transform.abstract_node_observer import AbstractNodeObserver
+from tei_transform.element_transformation import create_new_element
 
 
 class LonelyRowObserver(AbstractNodeObserver):
+    """
+    Observer for <row/> elements outside <table/>.
+
+    """
+
+    _new_table: Optional[etree._Element] = None
+
     def observe(self, node: etree._Element) -> bool:
         if etree.QName(node).localname == "row":
             parent = node.getparent()
@@ -11,4 +22,23 @@ class LonelyRowObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        parent = node.getparent()
+        if (
+            len(node) == 0
+            and (node.text is None or not node.text.strip())
+            and (node.tail is None or not node.tail.strip())
+        ):
+            parent.remove(node)
+            return
+        prev_sibling = node.getprevious()
+        if prev_sibling is None or prev_sibling != self._new_table:
+            new_table = create_new_element(node, "table")
+            parent.insert(parent.index(node), new_table)
+            self._new_table = new_table
+        self._new_table.append(node)
+        if node.tail is not None and node.tail.strip():
+            if self._new_table.tail is None:
+                self._new_table.tail = node.tail.strip()
+            else:
+                self._new_table.tail += f" {node.tail.strip()}"
+            node.tail = None
