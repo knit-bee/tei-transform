a74ed4e931a6307ac98c62177609acbf097b610d
Luise Koehler <luise.koehler@bbaw.de>
2023-01-18 14:57:07 +0100

    Implement element transformation for DoublePlikeObserver

--- tei_transform/observer/double_plike_observer.py
+++ tei_transform/observer/double_plike_observer.py
@@ -6,6 +6,10 @@ from tei_transform.abstract_node_observer import AbstractNodeObserver
 class DoublePlikeObserver(AbstractNodeObserver):
     """
     Observer for <p/> and <ab/> elements with parent <p/> or <ab/>.
+
+    Find <p/> and <ab/> elements that have <p/> or <ab/> as parent
+    and strip the inner tag (text and tail as well as the children
+    of the inner element will be preserved).
     """
 
     def observe(self, node: etree._Element) -> bool:
@@ -17,4 +21,30 @@ class DoublePlikeObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        parent = node.getparent()
+        prev_sibling = node.getprevious()
+        last_child = node[-1] if len(node) != 0 else None
+        self._pad_text_content_with_whitespace(node)
+        node.tag = "tempRename"
+        etree.strip_tags(parent, "tempRename")
+        # remove multiple whitespace and newline from xml formatting
+        self._strip_multiple_whitespaces_from_text_content(parent)
+        if prev_sibling is not None:
+            self._strip_multiple_whitespaces_from_text_content(prev_sibling, text=False)
+        if last_child is not None:
+            self._strip_multiple_whitespaces_from_text_content(last_child, text=False)
+
+    def _strip_multiple_whitespaces_from_text_content(
+        self, node: etree._Element, text: bool = True
+    ) -> None:
+        if text is True and node.text is not None:
+            node.text = " ".join(node.text.split())
+        else:
+            if node.tail is not None:
+                node.tail = " ".join(node.tail.split())
+
+    def _pad_text_content_with_whitespace(self, node: etree._Element) -> None:
+        if node.text is not None:
+            node.text = " " + node.text
+        if node.tail is not None:
+            node.tail = " " + node.tail
--- tests/test_double_plike_observer.py
+++ tests/test_double_plike_observer.py
@@ -5,7 +5,7 @@ from lxml import etree
 from tei_transform.observer import DoublePlikeObserver
 
 
-class ObserverTester(unittest.TestCase):
+class DoublePlikeObserverTester(unittest.TestCase):
     def setUp(self):
         self.observer = DoublePlikeObserver()
 
@@ -95,3 +95,221 @@ class ObserverTester(unittest.TestCase):
             result = {self.observer.observe(node) for node in element.iter()}
             with self.subTest():
                 self.assertEqual(result, {False})
+
+    def test_inner_element_removed(self):
+        root = etree.XML("<div><p><p/></p></div>")
+        node = root[0][0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//p/p") is None)
+
+    def test_text_of_inner_node_not_deleted(self):
+        root = etree.XML("<div><ab>text1<p>text2</p></ab></div>")
+        node = root[0][0]
+        self.observer.transform_node(node)
+        self.assertTrue("text2" in root[0].text)
+
+    def test_text_parts_separated_by_whitespace_after_concatenation(self):
+        root = etree.XML("<div><p>text1<ab>text2</ab>text3</p></div>")
+        node = root.find(".//ab")
+        self.observer.transform_node(node)
+        self.assertEqual(root[0].text, "text1 text2 text3")
+
+    def test_formatting_whitespace_removed_after_concatenation(self):
+        root = etree.XML(
+            """
+            <div>
+              <p>text1
+                <ab>text2</ab>text3
+                <p>text4</p>text5
+                <p>
+                  <ab>
+                    <p>text6</p>
+                  </ab>
+                </p>
+              </p>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        self.assertEqual(root[0].text, "text1 text2 text3 text4 text5 text6")
+
+    def test_tail_of_inner_element_not_deleted(self):
+        root = etree.XML("<div><p><p>text</p>tail</p></div>")
+        node = root[0][0]
+        self.observer.transform_node(node)
+        self.assertTrue("tail" in root[0].text)
+
+    def test_children_of_inner_element_not_deleted(self):
+        root = etree.XML("<div><p>text<ab>text<list/><table/><fw/></ab></p></div>")
+        node = root[0][0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//p/list") is not None)
+        self.assertEqual(len(root[0]), 3)
+
+    def test_inner_plike_removed_with_allowed_siblings(self):
+        root = etree.XML("<div><p>text<list/>tail<ab>text2</ab>tail2<table/></p></div>")
+        node = root.find(".//ab")
+        self.observer.transform_node(node)
+        result = [(node.tag, node.text, node.tail) for node in root[0].iter()]
+        self.assertEqual(
+            result,
+            [
+                ("p", "text", None),
+                ("list", None, "tail text2 tail2"),
+                ("table", None, None),
+            ],
+        )
+
+    def test_order_of_text_and_siblings_of_inner_element(self):
+        root = etree.XML(
+            "<div><p>text1<ab>text2<hi/>tail1<list/><table/></ab>tail2</p></div>"
+        )
+        node = root[0][0]
+        self.observer.transform_node(node)
+        result = [(node.tag, node.text, node.tail) for node in root[0].iter()]
+        self.assertEqual(
+            result,
+            [
+                ("p", "text1 text2", None),
+                ("hi", None, "tail1"),
+                ("list", None, None),
+                ("table", None, "tail2"),
+            ],
+        )
+
+    def test_inner_element_removed_with_namespace(self):
+        root = etree.XML("<TEI xmlns='ns'><div><p>text<ab>text2</ab></p></div></TEI>")
+        node = root.find(".//{*}ab")
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//{*}ab") is None)
+
+    def test_text_of_inner_node_not_deleted_with_namespace(self):
+        root = etree.XML("<TEI xmlns='ns'><div><p>text<ab>text2</ab></p></div></TEI>")
+        node = root.find(".//{*}ab")
+        self.observer.transform_node(node)
+        self.assertTrue("text2" in root.find(".//{*}p").text)
+
+    def test_tail_of_inner_element_not_deleted_with_namespace(self):
+        root = etree.XML("<TEI xmlns='ns'><div><p>text<ab/>tail</p></div></TEI>")
+        node = root.find(".//{*}ab")
+        self.observer.transform_node(node)
+        self.assertTrue("tail" in root.find(".//{*}p").text)
+
+    def test_children_of_inner_element_not_deleted_with_namespace(self):
+        root = etree.XML(
+            """
+            <TEI xmlns='ns'>
+              <div>
+                <p>text
+                  <ab>
+                    <list>
+                      <item>item</item>
+                    </list>
+                  </ab>
+                </p>
+              </div>
+            </TEI>
+            """
+        )
+        node = root.find(".//{*}ab")
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//{*}p//{*}list") is not None)
+
+    def test_removal_of_multiple_inner_elements(self):
+        root = etree.XML(
+            """
+            <div>
+              <ab>
+                <ab>
+                  <seg>text</seg>
+                </ab>
+                <p>
+                  <list>
+                    <item>text</item>
+                  </list>
+                </p>tail1
+                <p>text</p>tail2
+              </ab>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        result = [node.tag for node in root[0].iter()]
+        self.assertEqual(result, ["ab", "seg", "list", "item"])
+
+    def test_multiple_nested_plike_elements_resolved(self):
+        root = etree.XML(
+            """
+            <div>
+              <p>text1
+                <ab>text2
+                  <p>text3</p>
+                  <ab>
+                    <p>text4</p>
+                  </ab>
+                </ab>
+                <ab>
+                  <p>text5
+                    <p>text6
+                      <p>text7</p>
+                    </p>
+                  </p>
+                </ab>
+              </p>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        self.assertEqual(len(root[0]), 0)
+
+    def test_valid_nested_plike_element_not_removed(self):
+        root = etree.XML(
+            """
+            <div>
+              <p>
+                <ab>text</ab>
+                <list>
+                  <item>
+                    <p>text1</p>
+                    <ab>text2</ab>
+                  </item>
+                </list>
+                <p>text3</p>
+              </p>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        self.assertEqual(len(root.find(".//item")), 2)
+
+    def test_text_of_removed_element_added_as_tail_of_previous_sibling(self):
+        root = etree.XML("<div><p><list/><ab>text</ab></p></div>")
+        node = root[0][1]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list").tail, "text")
+
+    def test_tail_of_removed_element_added_as_tail_of_previous_sibling(self):
+        root = etree.XML("<div><p><list/><ab/>tail</p></div>")
+        node = root[0][1]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list").tail, "tail")
+
+    def test_tail_of_removed_element_added_as_tail_of_last_child(self):
+        root = etree.XML("<div><ab><list/><p><hi>text</hi></p>tail</ab></div>")
+        node = root.find(".//p")
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//hi").tail, "tail")
+
+    def test_text_of_removed_element_added_as_tail_of_prev_sibling_with_children(self):
+        root = etree.XML("<div><ab><list/><p>text<hi>text2</hi></p></ab></div>")
+        node = root.find(".//p")
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list").tail, "text")
