63ca4903ccd219067ddc2e2e674532cd089a4366
Luise Koehler <luise.koehler@bbaw.de>
2023-01-23 12:31:31 +0100

    Refactor PAsDivSiblingObserver
    - Add __init__ method
    - Add check to reset self._new_element if it has <div/> children

--- tei_transform/observer/p_as_div_sibling_observer.py
+++ tei_transform/observer/p_as_div_sibling_observer.py
@@ -16,7 +16,8 @@ class PAsDivSiblingObserver(AbstractNodeObserver):
     <p/> element is empty, it is removed.
     """
 
-    _new_element: Optional[list] = None
+    def __init__(self) -> None:
+        self._new_element: Optional[etree._Element] = None
 
     def observe(self, node: etree._Element) -> bool:
         if etree.QName(node).localname == "p":
@@ -30,15 +31,18 @@ class PAsDivSiblingObserver(AbstractNodeObserver):
             or (node.text is not None and node.text.strip())
             or (node.tail is not None and node.tail.strip())
         ):
+            self._reset_last_created_new_div_if_necessary()
             sibling = node.getprevious()
-            if sibling is not None:
-                if self._new_element is sibling:
-                    self._new_element.append(node)
-                else:
-                    new_element = create_new_element(node, "div")
-                    parent = sibling.getparent()
-                    parent.insert(parent.index(node), new_element)
-                    self._new_element = new_element
-                    new_element.append(node)
+            if sibling is None or sibling != self._new_element:
+                new_element = create_new_element(node, "div")
+                parent = node.getparent()
+                parent.insert(parent.index(node), new_element)
+                self._new_element = new_element
+            self._new_element.append(node)
         else:
             node.getparent().remove(node)
+
+    def _reset_last_created_new_div_if_necessary(self) -> None:
+        if self._new_element is not None:
+            if list(self._new_element.iterchildren("{*}div")) != []:
+                self._new_element = None
