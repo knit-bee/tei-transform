9b2260c84e14003a2e974846998ed568c430d77e
Luise Koehler <luise.koehler@bbaw.de>
2023-01-24 16:03:00 +0100

    Implement element transformation for LonelyItemObserver

--- tei_transform/observer/lonely_item_observer.py
+++ tei_transform/observer/lonely_item_observer.py
@@ -1,12 +1,18 @@
+from typing import Optional
+
 from lxml import etree
 
 from tei_transform.abstract_node_observer import AbstractNodeObserver
+from tei_transform.element_transformation import create_new_element
 
 
 class LonelyItemObserver(AbstractNodeObserver):
     """
     Observer for <item/> elements outside <list/> elements.
     """
+
+    def __init__(self) -> None:
+        self._new_list: Optional[etree._Element] = None
 
     def observe(self, node: etree._Element) -> bool:
         if etree.QName(node).localname == "item" and etree.QName(
@@ -16,4 +22,33 @@ class LonelyItemObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        parent = node.getparent()
+        if (
+            len(node) == 0
+            and (node.text is None or not node.text.strip())
+            and (node.tail is None or not node.tail.strip())
+        ):
+            parent.remove(node)
+            return
+        prev_sibling = node.getprevious()
+        if prev_sibling is None or prev_sibling != self._new_list:
+            new_list = create_new_element(node, "list")
+            parent.insert(parent.index(node), new_list)
+            self._new_list = new_list
+        self._new_list.append(node)
+        if node.tail is not None and node.tail.strip():
+            self._move_tail_of_item(node)
+
+    def _move_tail_of_item(self, node: etree._Element) -> None:
+        if len(node) != 0:
+            last_subchild = node[-1]
+            if last_subchild.tail is None:
+                last_subchild.tail = node.tail
+            else:
+                last_subchild.tail += f" {node.tail}"
+        else:
+            if node.text is not None:
+                node.text = " ".join([node.text, node.tail])
+            else:
+                node.text = node.tail
+        node.tail = None
--- tests/test_lonely_item_observer.py
+++ tests/test_lonely_item_observer.py
@@ -139,3 +139,186 @@ class LonelyItemObserverTester(unittest.TestCase):
             result = {self.observer.observe(node) for node in element.iter()}
             with self.subTest():
                 self.assertEqual(result, {False})
+
+    def test_list_added_as_parent(self):
+        root = etree.XML("<p><item>text</item></p>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//list/item") is not None)
+
+    def test_list_added_as_parent_with_namespace(self):
+        root = etree.XML("<TEI xmlns='a'><div><p><item>text</item></p></div></TEI>")
+        node = root.find(".//{*}item")
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//{*}list/{*}item") is not None)
+
+    def test_empty_item_removed(self):
+        root = etree.XML("<p>text<item/></p>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(len(root), 0)
+
+    def test_empty_item_with_tail_not_removed(self):
+        root = etree.XML("<p><item/>tail</p>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//list/item") is not None)
+
+    def test_empty_item_with_text_not_removed(self):
+        root = etree.XML("<p><item>text</item></p>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//list/item") is not None)
+
+    def test_empty_item_with_only_whitespace_text_removed(self):
+        root = etree.XML("<div><item>   \n\n</item></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//item") is None)
+
+    def test_empty_item_with_only_whitespace_tail_removed(self):
+        root = etree.XML("<ab><item/>\n   \n </ab>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//item") is None)
+
+    def test_attributes_of_item_preserved_after_transformation(self):
+        root = etree.XML("<div><item rend='val'>text</item><p/></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list/item").attrib, {"rend": "val"})
+
+    def test_attributes_of_item_preserved_after_transformation_with_namespace(self):
+        root = etree.XML(
+            "<TEI xmlns='a'><div><item rend='val'>text</item><p/></div></TEI>"
+        )
+        node = root.find(".//{*}item")
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//{*}list/{*}item").attrib, {"rend": "val"})
+
+    def test_children_preserved_after_transformation(self):
+        root = etree.XML(
+            """
+            <div>
+              <p>text</p>
+              <item>
+                <head>text</head>
+                <list>
+                  <item>text</item>
+                  <item><p>ab</p></item>
+                  <item><hi>cd</hi></item>
+                </list>
+              </item>
+            </div>
+            """
+        )
+        node = root.find(".//item")
+        self.observer.transform_node(node)
+        result = [node.tag for node in root.find(".//list/item").iter()]
+        self.assertEqual(
+            result, ["item", "head", "list", "item", "item", "p", "item", "hi"]
+        )
+
+    def test_multiple_adjacent_items_added_to_same_list_element(self):
+        root = etree.XML(
+            """
+            <div>
+              <item>ab</item>
+              <item>cd</item>
+              <item>ef</item>
+              <item>gh</item>
+              <item>ij</item>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        result = [len(root), len(root[0])]
+        self.assertEqual(result, [1, 5])
+
+    def test_multiple_adjacent_items_added_to_same_list_element_with_namespace(self):
+        root = etree.XML(
+            """
+            <TEI xmlns='a'>
+              <teiHeader/>
+              <text>
+                <body>
+                  <div>
+                  <p>text</p>
+                  <item>text</item>
+                  <item>ab</item>
+                  <item>cd</item>
+                  </div>
+                </body>
+              </text>
+            </TEI>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        result = [len(root.find(".//{*}div")), len(root.find(".//{*}list"))]
+        self.assertEqual(result, [2, 3])
+
+    def test_transformation_only_applied_to_outer_item_if_nested(self):
+        root = etree.XML(
+            """
+            <div>
+              <item>
+                <item>text</item>
+              </item>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        self.assertTrue(root.find(".//list/item/item") is not None)
+
+    def test_non_item_siblings_not_added_to_list_parent(self):
+        root = etree.XML(
+            """
+            <div>
+              <p>text</p>
+              <item>ab</item>
+              <item>cd</item>
+              <p>text</p>
+              <item>ef</item>
+              <item/>
+              <p>text</p>
+              <item>gh</item>
+              <item>ij</item>
+            </div>
+            """
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        list_elements = root.findall(".//list")
+        result = [len(list_elements), [len(elem) for elem in list_elements]]
+        self.assertEqual(result, [3, [2, 1, 2]])
+
+    def test_tail_from_item_removed(self):
+        root = etree.XML("<div><item/>tail</div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list/item").tail, None)
+
+    def test_tail_from_item_added_to_text_context(self):
+        root = etree.XML("<div><item>text</item>tail</div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list/item").text, "text tail")
+
+    def test_tail_added_as_text_if_item_empty(self):
+        root = etree.XML("<div><item/>tail</div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//list/item").text, "tail")
+
+    def test_tail_added_to_tail_of_last_child_of_item(self):
+        root = etree.XML("<p><item><table/>old tail</item>tail</p>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(root.find(".//table").tail, "old tail tail")
