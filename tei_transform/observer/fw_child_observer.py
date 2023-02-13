8c557cdb119433084d0a086acca7e138bc9029bf
Luise Koehler <luise.koehler@bbaw.de>
2023-02-13 14:02:30 +0100

    Add <table/> to target elements of FwChildObserver

--- tei_transform/observer/fw_child_observer.py
+++ tei_transform/observer/fw_child_observer.py
@@ -6,26 +6,26 @@ from tei_transform.element_transformation import change_element_tag, merge_into_
 
 class FwChildObserver(AbstractNodeObserver):
     """
-    Observer for <p/> and <list/> elements with <fw/> parent.
+    Observer for <p/>, <list/>, and <table/> elements with <fw/> parent.
 
-    Find <p/> and <list/> elements that are children of <fw/>.
+    Find <p/>, <list/>, <table/> elements that are children of <fw/>.
 
     If the target element has tag 'p', it is merge into the
     parent by stripping the inner element (text, tail, and
     children are not removed).
 
-    If the target element has tag 'list', the tag of the
-    parent is changed to 'ab'.
+    If the target element has tag 'list' or 'table', the tag of
+    the parent is changed to 'ab'.
     N.B.:
     - Use in combination with DoublePlikeObserver to
     avoid invalid structure if the <fw/> parent contains
-    <p/> and <list/>.
+    <p/> and <list/> /<table/>.
     - Use in combination with NestedFwWithListObserver to avoid
     nesting of <fw/> and <ab/>.
     """
 
     def observe(self, node: etree._Element) -> bool:
-        target_tags = {"p", "list"}
+        target_tags = {"p", "list", "table"}
         if etree.QName(node).localname in target_tags:
             parent = node.getparent()
             if parent is not None and etree.QName(parent).localname == "fw":
@@ -35,5 +35,5 @@ class FwChildObserver(AbstractNodeObserver):
     def transform_node(self, node: etree._Element) -> None:
         if etree.QName(node).localname == "p":
             merge_into_parent(node)
-        if etree.QName(node).localname == "list":
+        else:
             change_element_tag(node.getparent(), "ab")
