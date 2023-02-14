015b966829ddee078a43c591d22b32b8c173dd56
Luise Koehler <luise.koehler@bbaw.de>
2023-02-14 12:24:56 +0100

    Implement element transformation for UnfinishedElementObserver

--- tei_transform/observer/unfinished_element_observer.py
+++ tei_transform/observer/unfinished_element_observer.py
@@ -1,6 +1,7 @@
 from lxml import etree
 
 from tei_transform.abstract_node_observer import AbstractNodeObserver
+from tei_transform.element_transformation import create_new_element
 
 
 class UnfinishedElementObserver(AbstractNodeObserver):
@@ -21,4 +22,25 @@ class UnfinishedElementObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        if etree.QName(node).localname == "list":
+            self._handle_list(node)
+        if etree.QName(node).localname == "table":
+            self._handle_table(node)
+
+    def _handle_list(self, node: etree._Element) -> None:
+        new_item = create_new_element(node, "item")
+        insert_index = 0
+        for child in node.iterchildren(["{*}head", "{*}desc"], reversed=True):
+            insert_index = node.index(child) + 1
+            break
+        node.insert(insert_index, new_item)
+
+    def _handle_table(self, node: etree._Element) -> None:
+        new_row = create_new_element(node, "row")
+        new_cell = create_new_element(node, "cell")
+        insert_index = 0
+        for child in node.iterchildren("{*}head", reversed=True):
+            insert_index = node.index(child) + 1
+            break
+        new_row.append(new_cell)
+        node.insert(insert_index, new_row)
