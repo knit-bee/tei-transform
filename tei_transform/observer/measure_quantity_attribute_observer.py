434f58b1358101e198029f76d1d82caf4c1ee7d2
Luise Köhler <koehler@bbaw.de>
2023-02-27 14:48:32 +0100

    Update docstring

--- tei_transform/observer/measure_quantity_attribute_observer.py
+++ tei_transform/observer/measure_quantity_attribute_observer.py
@@ -7,6 +7,9 @@ from tei_transform.element_transformation import remove_attribute_from_node
 class MeasureQuantityAttributeObserver(AbstractNodeObserver):
     """
     Observer for <term/> elements with @measure_quantity attribute.
+
+    Find <term/> elements with attribute @measure_quantity and remove
+    attribute.
     """
 
     def observe(self, node: etree._Element) -> bool:
