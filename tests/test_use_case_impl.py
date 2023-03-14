91679ef80238d59600653d5bea19c5d0105f9343
Luise Koehler <luise.koehler@bbaw.de>
2023-03-14 12:00:29 +0100

    Test combination of empty-body and body-text

--- tests/test_use_case_impl.py
+++ tests/test_use_case_impl.py
@@ -933,6 +933,16 @@ class UseCaseTester(unittest.TestCase):
         )
         self.assertTrue(result)
 
+    def test_combination_of_empty_body_and_body_text(self):
+        plugins = ["body-text", "empty-body"]
+        for plugins_to_use in permutations(plugins):
+            with self.subTest():
+                self.assertTrue(
+                    self._validate_file_processed_with_plugins(
+                        "file_with_body_with_text_and_children.xml", plugins_to_use
+                    )
+                )
+
     def file_invalid_because_classcode_misspelled(self, file):
         logs = self._get_validation_error_logs_for_file(file)
         expected_error_msg = "Did not expect element classcode there"
