1c1e0f0f373385f3f7e08a1da0dae50e30b6b07e
Luise Koehler <luise.koehler@bbaw.de>
2023-03-30 12:02:52 +0200

    Implement element transformation for MissingBodyObserver

--- tei_transform/observer/missing_body_observer.py
+++ tei_transform/observer/missing_body_observer.py
@@ -1,6 +1,7 @@
 from lxml import etree
 
 from tei_transform.abstract_node_observer import AbstractNodeObserver
+from tei_transform.element_transformation import create_new_element
 
 
 class MissingBodyObserver(AbstractNodeObserver):
@@ -8,7 +9,13 @@ class MissingBodyObserver(AbstractNodeObserver):
     Observer for <text/> elements missing required children.
 
     Find <text/> elements that don't have any children with
-    tags 'body' or 'group'.
+    tags 'body' or 'group'. A new <body/> element is added
+    to the <text/> element and all children of <text/> that
+    come between <front/> and <back/>, if present, are added
+    as children to the new <body/> element.
+    The <body/> element is inserted as first child of <text/>
+    or, if a <front/> element is found, after the <front/>
+    element.
     """
 
     def observe(self, node: etree._Element) -> bool:
@@ -25,4 +32,22 @@ class MissingBodyObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        body_elem = create_new_element(node, "body")
+        front_elem = node.find("{*}front")
+        children_to_add = []
+        if front_elem is None:
+            insert_index = 0
+            candidates_to_move = (child for child in node)
+        else:
+            insert_index = node.index(front_elem) + 1
+            candidates_to_move = (sibling for sibling in front_elem.itersiblings())
+        for elem in candidates_to_move:
+            if etree.QName(elem).localname == "back":
+                break
+            children_to_add.append(elem)
+        node.insert(insert_index, body_elem)
+        body_elem.extend(children_to_add)
+        # prevent TEI error if body is empty by adding <p/>
+        if len(body_elem) == 0:
+            new_p = create_new_element(node, "p")
+            body_elem.append(new_p)
--- tests/test_missing_body_observer.py
+++ tests/test_missing_body_observer.py
@@ -61,3 +61,66 @@ class MissingBodyObserverTester(unittest.TestCase):
             result = {self.observer.observe(node) for node in element.iter()}
             with self.subTest():
                 self.assertEqual(result, {False})
+
+    def test_body_added_as_child_of_text(self):
+        root = etree.XML("<TEI><text/></TEI>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//body") is not None)
+
+    def test_body_added_as_child_of_text_with_namespace(self):
+        root = etree.XML("<TEI xmlns='a'><text/></TEI>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertTrue(root.find(".//{*}body") is not None)
+
+    def test_body_added_during_iteration(self):
+        root = etree.XML(
+            """
+        <TEI>
+          <teiHeader/>
+          <text>
+            <p>ab</p>
+            <p/>
+          </text>
+        </TEI>"""
+        )
+        for node in root.iter():
+            if self.observer.observe(node):
+                self.observer.transform_node(node)
+        self.assertEqual(len(root.find(".//text")), 1)
+
+    def test_children_of_text_added_to_body(self):
+        root = etree.XML("<text><p/><ab/><p/></text>")
+        self.observer.transform_node(root)
+        result = [child.tag for child in root[0]]
+        self.assertEqual(["p", "ab", "p"], result)
+
+    def test_children_of_text_removed_unless_front_or_back(self):
+        root = etree.XML("<text><front/><p/><ab/><p/><back/></text>")
+        self.observer.transform_node(root)
+        result = [child.tag for child in root]
+        self.assertEqual(["front", "body", "back"], result)
+
+    def test_back_not_added_to_body(self):
+        root = etree.XML("<text><p/><back/></text>")
+        self.observer.transform_node(root)
+        result = [node.tag for node in root]
+        self.assertEqual(["body", "back"], result)
+
+    def test_front_not_added_to_body(self):
+        root = etree.XML("<text><front/><p/></text>")
+        self.observer.transform_node(root)
+        result = [node.tag for node in root]
+        self.assertEqual(["front", "body"], result)
+
+    def test_body_added_after_front_sibling(self):
+        root = etree.XML("<text><front/><p/><back/></text>")
+        self.observer.transform_node(root)
+        result = [node.tag for node in root]
+        self.assertEqual(["front", "body", "back"], result)
+
+    def test_empty_p_added_to_new_body_if_empty_otherwise(self):
+        root = etree.XML("<text><front/><back/></text>")
+        self.observer.transform_node(root)
+        self.assertTrue(root.find(".//body/p") is not None)
