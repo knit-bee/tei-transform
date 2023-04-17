3c187ce52c07c721f0178fc515f6e2e678cd5105
Luise Koehler <luise.koehler@bbaw.de>
2023-04-17 16:49:53 +0200

    Test misused-l plugin on file

--- tests/test_use_case_impl.py
+++ tests/test_use_case_impl.py
@@ -1064,6 +1064,12 @@ class UseCaseTester(unittest.TestCase):
         )
         self.assertTrue(result)
 
+    def test_misused_l_resolved(self):
+        result = self._validate_file_processed_with_plugins(
+            "file_with_l_in_s.xml", ["misused-l"]
+        )
+        self.assertTrue(result)
+
     def file_invalid_because_classcode_misspelled(self, file):
         logs = self._get_validation_error_logs_for_file(file)
         expected_error_msg = "Did not expect element classcode there"
