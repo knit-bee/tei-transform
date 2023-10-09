a129a81ce6c3b5f816b274fd397a3f222a46a2cd
Luise Köhler <koehler@bbaw.de>
2023-10-09 21:41:07 +0200

    Implement element transformation for HLevelObserver

--- tei_transform/observer/h_level_observer.py
+++ tei_transform/observer/h_level_observer.py
@@ -1,11 +1,19 @@
 from lxml import etree
 
 from tei_transform.abstract_node_observer import AbstractNodeObserver
+from tei_transform.element_transformation import (
+    change_element_tag,
+    remove_attribute_from_node,
+)
 
 
 class HLevelObserver(AbstractNodeObserver):
     """
     Observer for <h#/> elements.
+
+    Change tag of elements with 'h#' tag (e.g. 'h2') to 'ab' and add
+    @type='head' and @rend with the old tag name as value.
+    Invalid attributes, such as @class and @title, are removed.
     """
 
     def observe(self, node: etree._Element) -> bool:
@@ -15,4 +23,10 @@ class HLevelObserver(AbstractNodeObserver):
         return False
 
     def transform_node(self, node: etree._Element) -> None:
-        pass
+        old_tag = etree.QName(node).localname
+        change_element_tag(node, "ab")
+        node.set("type", "head")
+        node.set("rend", old_tag)
+        unwanted_attributes = ["class", "title"]
+        for attr in unwanted_attributes:
+            remove_attribute_from_node(node, attr)
--- tests/test_h_level_observer.py
+++ tests/test_h_level_observer.py
@@ -63,3 +63,71 @@ class HLevelObserverTester(unittest.TestCase):
             result = {self.observer.observe(node) for node in element.iter()}
             with self.subTest():
                 self.assertEqual(result, {False})
+
+    def test_tag_changed_to_ab(self):
+        root = etree.XML("<div><h3>text</h3></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(node.tag, "ab")
+
+    def test_tag_changed_to_ab_with_namespace(self):
+        root = etree.XML(
+            "<TEI xmlns='http://www.tei-c.org/ns/1.0'><div><h4>text</h4></div></TEI>"
+        )
+        node = root.find(".//{*}h4")
+        self.observer.transform_node(node)
+        self.assertEqual(etree.QName(node).localname, "ab")
+        self.assertEqual(etree.QName(node).namespace, "http://www.tei-c.org/ns/1.0")
+
+    def test_type_attribute_added(self):
+        root = etree.XML("<div><h2>text</h2><p/></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(node.attrib.get("type"), "head")
+
+    def test_rend_attribute_added_with_old_tag_name(self):
+        for i in range(2, 9):
+            tag_name = f"h{i}"
+            root = etree.XML(f"<div><{tag_name}>text</{tag_name}></div>")
+            node = root[0]
+            self.observer.transform_node(node)
+            with self.subTest():
+                self.assertEqual(node.attrib.get("rend"), tag_name)
+
+    def test_rend_attribute_added_with_old_tag_name_with_namespace(self):
+        for i in range(2, 9):
+            tag_name = f"h{i}"
+            root = etree.XML(
+                f"<TEI xmlns='http://www.tei-c.org/ns/1.0'><div><p/><{tag_name}>text</{tag_name}></div></TEI>"
+            )
+            node = root.find(".//{*}div")[1]
+            self.observer.transform_node(node)
+            with self.subTest():
+                self.assertEqual(node.attrib.get("rend"), tag_name)
+
+    def test_unwanted_attributes_removed(self):
+        root = etree.XML("<div><h2 class='sth' title='a'>text</h2></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(node.attrib, {"type": "head", "rend": "h2"})
+
+    def test_other_attributes_not_removed(self):
+        root = etree.XML("<div><h4 xml:id='sth' class='a' n='a' rendition='#i'/></div>")
+        node = root[0]
+        self.observer.transform_node(node)
+        self.assertEqual(
+            (node.attrib),
+            {
+                "{http://www.w3.org/XML/1998/namespace}id": "sth",
+                "n": "a",
+                "rendition": "#i",
+                "rend": "h4",
+                "type": "head",
+            },
+        )
+
+    def test_element_with_children_renamed(self):
+        root = etree.XML("<div><p/><h3><lb/>text</h3><p/></div>")
+        node = root.find("h3")
+        self.observer.transform_node(node)
+        self.assertEqual(len(node), 1)
